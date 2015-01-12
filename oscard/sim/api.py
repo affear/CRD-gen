d1a2bfd76583e12c69e2ec5f6e12bbb32951cbd1
Lorenzo Affetti <affox90@gmail.com>
2015-01-12 10:41:21 +0100

    fakeapi added

--- oscard/sim/api.py
+++ oscard/sim/api.py
@@ -3,17 +3,21 @@ from oscard import log
 
 LOG = log.get_logger(__name__)
 
-class NovaAPI(object):
-	_baseurl = ''
+class FakeAPI(object):
 
 	def create(self, **kwargs):
 		LOG.info("novapi: create")
-		return {"response": "OK", "status": "200"}
+		return {"body": "created"}, 200
 
 	def resize(self, **kwargs):
 		LOG.info("novapi: resize")
-		return {"response": "OK", "status": "200"}
+		return {"body": "resized"}, 200
 
 	def destroy(self, **kwargs):
 		LOG.info("novapi: destroy")
-		return {"response": "OK", "status": "200"}
+		return {"body": "destroyed"}, 200
+
+class NovaAPI(object):
+	_baseurl = ''
+
+	pass
