b980a4816449cea83f987c161451618715c1d262
Lorenzo Affetti <affox90@gmail.com>
2015-01-23 11:18:53 +0100

    v1

--- oscard/sim/collector.py
+++ oscard/sim/collector.py
@@ -1,10 +1,11 @@
 from firebase import firebase
 from oslo.config import cfg
+import datetime
 
 bifrost_opts = [
 	cfg.StrOpt(
-		name='bifrost_fb_url',
-		default='http://bifrosturl.fake.firebase.com',
+		name='fb_backend',
+		default='https://fake.url.firebaseio.com',
 		help='Bifrost app url on Firebase'
 	)
 ]
@@ -13,17 +14,44 @@ CONF = cfg.CONF
 CONF.register_opts(bifrost_opts)
 
 class BifrostAPI(object):
-	app = firebase.FirebaseApplication(CONF.bifrost_fb_url, None)
+	app = firebase.FirebaseApplication(CONF.fb_backend, None)
 
 	@property
 	def seed(self):
-		pass
+		return self.app.get('/last_sim_id', None)
 
-	def seedpp(self):
-		pass
+	def __init__(self):
+		if not self.seed:
+			self.app.put('/', 'last_sim_id', 0)
 
-	def add_snapshot(self, seed, snapshot):
-		pass
+	def _seedpp(self):
+		last_id = self.seed + 1
+		self.app.put('/', 'last_sim_id', last_id)
+		return last_id
 
-	def add_sim(self, seed):
-		pass
+	def add_snapshot(self, step, command_name, snapshot, sim_id=None):
+		if not sim_id:
+			sim_id = self.seed
+
+		data = {
+			'command': command_name,
+			'hosts': snapshot
+		}
+
+		base_url = '/sims/' + str(sim_id) + '/snapshots'
+		return self.app.put(base_url, str(step), data)
+
+	def add_sim(self, steps, type, id=None, created_at=None):
+		if not id:
+			id = self._seedpp()
+
+		if not created_at:
+			created_at = str(datetime.datetime.now())
+
+		data = {
+			'steps': steps,
+			'type': type,
+			'created_at': created_at,
+		}
+
+		return self.app.put('/sims', str(id), data)
