26560d3f7a69be0df134c0cd701c86e05712c646
Lorenzo Affetti <affox90@gmail.com>
2015-01-26 14:33:51 +0100

    bifrost methods are now celery tasks

--- oscard/sim/collector.py
+++ oscard/sim/collector.py
@@ -1,6 +1,9 @@
 from firebase import firebase
+from celery.contrib.methods import task
+from celery import Celery
 from oslo.config import cfg
 import datetime
+from oscard import config, log
 
 bifrost_opts = [
 	cfg.StrOpt(
@@ -12,15 +15,24 @@ bifrost_opts = [
 
 CONF = cfg.CONF
 CONF.register_opts(bifrost_opts)
+LOG = log.get_logger(__name__)
+
+cel = Celery('bifrost_tasks', backend='amqp', broker='amqp://guest@localhost//')
 
 class BifrostAPI(object):
-	app = firebase.FirebaseApplication(CONF.fb_backend, None)
 
 	@property
 	def seed(self):
 		return self.app.get('/last_sim_id', None)
 
 	def __init__(self):
+		# we have to init from configuration file
+		# in case the module is run from celery worker!
+		# we do it inside __init__ because of conflicts
+		# between oslo and celery worker...
+		config.init_conf()
+		self.app = firebase.FirebaseApplication(CONF.fb_backend, None)
+
 		if self.seed is None:
 			self.app.put('/', 'last_sim_id', -1)
 
@@ -32,6 +44,7 @@ class BifrostAPI(object):
 	def _parse_host(self, host_ip_port):
 		return host_ip_port.replace('.', '_').replace(':', '__')
 
+	@task(name='bifrost.add_failure')
 	def add_failure(self, host, step, f, sim_id=None):
 		if not sim_id:
 			sim_id = self.seed
@@ -46,6 +59,7 @@ class BifrostAPI(object):
 		base_url += '/failures'
 		return self.app.put(base_url, str(step), f)
 
+	@task(name='bifrost.add_snapshot')
 	def add_snapshot(self, host, step, command_name, snapshot, sim_id=None):
 		if not sim_id:
 			sim_id = self.seed
@@ -62,6 +76,7 @@ class BifrostAPI(object):
 
 		return self.app.put(base_url + '/snapshots', str(step), snapshot)
 
+	@task(name='bifrost.add_sim')
 	def add_sim(self, steps, hosts_dict, id=None, created_at=None):
 		'''
 			- steps: the number of steps
@@ -96,6 +111,7 @@ class BifrostAPI(object):
 
 		return self.app.put('/sims', str(id), data)
 
+	@task(name='bifrost.add_end')
 	def add_end_to_current_sim(self):
 		default_formatting = '%Y-%m-%d %H:%M:%S.%f'
 		id = self.seed
