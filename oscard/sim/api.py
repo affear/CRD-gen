3ebda4986b7e43f94702d860e83d335fa21391e8
Lorenzo Affetti <affox90@gmail.com>
2015-03-10 11:39:56 +0100

    snapshot avgs have to be calculate on all hosts, and not only on active ones

--- oscard/sim/api.py
+++ oscard/sim/api.py
@@ -431,8 +431,7 @@ class NovaAPI(CRDAPI):
 			'cmps': {}
 		}
 		hosts = self.nova.hypervisors.list()
-		# only active hosts
-		hosts = filter(lambda h: h.vcpus_used != 0, hosts)
+		no_hosts = len(hosts)
 
 		#####
 		# if you want to retrieve the host from the server:
@@ -461,17 +460,15 @@ class NovaAPI(CRDAPI):
 		avg_r_memory_mb = 0
 		avg_r_local_gb = 0
 
-		n_active_hosts = float(len(hosts))
-
-		if n_active_hosts > 0:
-			avg_r_vcpus = sum([ans['cmps'][h]['r_vcpus'] for h in ans['cmps']]) / n_active_hosts
-			avg_r_memory_mb = sum([ans['cmps'][h]['r_memory_mb'] for h in ans['cmps']]) / n_active_hosts
-			avg_r_local_gb = sum([ans['cmps'][h]['r_local_gb'] for h in ans['cmps']]) / n_active_hosts
+		if no_hosts > 0:
+			avg_r_vcpus = sum([ans['cmps'][h]['r_vcpus'] for h in ans['cmps']]) / float(no_hosts)
+			avg_r_memory_mb = sum([ans['cmps'][h]['r_memory_mb'] for h in ans['cmps']]) / float(no_hosts)
+			avg_r_local_gb = sum([ans['cmps'][h]['r_local_gb'] for h in ans['cmps']]) / float(no_hosts)
 
 		ans['avg_r_vcpus'] = avg_r_vcpus
 		ans['avg_r_memory_mb'] = avg_r_memory_mb
 		ans['avg_r_local_gb'] = avg_r_local_gb
-		ans['no_active_cmps'] = n_active_hosts
+		ans['no_active_cmps'] = len(filter(lambda h: h.vcpus_used > 0, hosts))
 
 		return ans
 
